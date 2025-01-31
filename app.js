49a436470996424ceebe4ebd05ba0e245663e90f
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2025-01-31 16:26:46 +0100

    fix: test import now works

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ const port = process.env.PORT;
 // #endregion variabili d'importazione
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+// impostazione temporanea rotta index
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
 
 // impostazione server
 app.listen(port, (req, res) => {
     console.log(`Server listening on: ${host}:${port}`);
 })
-
-app.use(cors());
