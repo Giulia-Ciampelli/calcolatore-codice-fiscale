d030bc09625ec150b3f046cbfdc098ba7febe604
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2025-01-31 13:40:14 +0100

    docs: link form elements

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// #region variabili
+let firstName = document.getElementById('first_name');
+let lastName = document.getElementById('last_name');
+let dateOfBirth = document.getElementById('date_of_birth');
+let sex = document.getElementById('sex');
+let municipality = document.getElementById('municipality');
+let country = document.getElementById('country');
+// #endregion variabili
+
+console.log('im alive');
