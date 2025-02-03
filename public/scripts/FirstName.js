a38c06e9ac18f292e912384f34d74504addeb4f8
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2025-02-03 09:44:44 +0100

    docs: try with regex and .match metohd

--- public/scripts/FirstName.js
+++ public/scripts/FirstName.js
@@ -1,3 +1,17 @@
+// Si prendono tutte le consonanti, se sono più di 3 si prendono la 1^, 3^ e 4^.
+// Se il nome ha meno di 3 consonanti, si prendono anche le vocali.
+// Se un nome ha meno di 3 lettere, si aggiunge una X alla fine.
+
 export default function FirstName() {
     console.log('import works!');
+
+    let nome = 'Gianmarco';
+    let regexSearch = nome.match(/[^aeiou]/gi); // trova tutto ciò che non è vocale
+
+    // RICORDA: il match rende un array, non una stringa!
+
+    let result = regexSearch.map(letter => letter.toUpperCase());
+
+    console.log(result);
+
 }
