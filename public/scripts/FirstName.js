f9e52dca67dd89069981afb6e7bddb48cdfd5a29
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2025-02-03 10:25:45 +0100

    refactor: switch case

--- public/scripts/FirstName.js
+++ public/scripts/FirstName.js
@@ -1,17 +1,46 @@
-// Si prendono tutte le consonanti, se sono più di 3 si prendono la 1^, 3^ e 4^.
-// Se il nome ha meno di 3 consonanti, si prendono anche le vocali.
-// Se un nome ha meno di 3 lettere, si aggiunge una X alla fine.
-
 export default function FirstName() {
     console.log('import works!');
 
-    let nome = 'Gianmarco';
+    let nome = 'Giulia';
     let regexSearch = nome.match(/[^aeiou]/gi); // trova tutto ciò che non è vocale
+    let regexVowels = nome.match(/[aeiou]/gi); // trova tutte le vocali
 
     // RICORDA: il match rende un array, non una stringa!
 
-    let result = regexSearch.map(letter => letter.toUpperCase());
+    let result = [];
 
+    switch (true) {
+        case (regexSearch.length > 3):
+
+            // Si prendono tutte le consonanti, se sono più di 3 si prendono la 1^, 3^ e 4^.
+            result = [
+                regexSearch[0],
+                regexSearch[2],
+                regexSearch[3]
+            ].map(letter => letter.toUpperCase());
+            break;
+
+        case (regexSearch.length === 3):
+
+            // Si prendono tutte le consonanti
+            result = regexSearch.map(letter => letter.toUpperCase());
+            break;
+
+        case (regexSearch.length < 3):
+
+            // Se il nome ha meno di 3 consonanti, si prendono anche le vocali.
+            let consonants = regexSearch.map(letter => letter.toUpperCase());
+            let vowels = [regexVowels[0]].map(letter => letter.toUpperCase());
+            result = [...consonants, ...vowels];
+            break;
+
+        default:
+
+            // Se un nome ha meno di 3 lettere, si aggiunge una X alla fine.
+            if (nome.length < 3) {
+                result = 'caso da sistemare';
+            }
+            break;
+    }
     console.log(result);
-
 }
